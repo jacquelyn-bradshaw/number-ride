6c83a1f01aed8d2d8dce56ea44b44f3d77d83ab2
jacquelyn-bradshaw <jacquelyn.bradshaw@flutteruki.com>
2025-01-03 15:13:29 +0000

    improve code by mapping the array

--- components/answerButtons/answerButtons.tsx
+++ components/answerButtons/answerButtons.tsx
@@ -13,12 +13,12 @@ export default function AnswerButtons({
     randomNumber(20),
     randomNumber(20),
   ];
+
   return (
     <div className={styles.buttons}>
-      <AnswerButton answer={possibleAnswers[0]} />
-      <AnswerButton answer={possibleAnswers[1]} />
-      <AnswerButton answer={possibleAnswers[2]} />
-      <AnswerButton answer={possibleAnswers[3]} />
+      {possibleAnswers.map((answer, index) => (
+        <AnswerButton key={index} answer={answer} />
+      ))}
     </div>
   );
 }
