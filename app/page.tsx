e4474881b74f302b81d17dfe8af47950a3b6a990
jacquelyn-bradshaw <jacquelyn.bradshaw@flutteruki.com>
2025-01-03 14:46:58 +0000

    home page styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div>
+      <div className={styles.container}>
         <h2>Addition</h2>
         <GameLink type="addition" amount={1} text="+1" />
         <GameLink type="addition" amount={2} text="+2" />
       </div>
-      <div>
+      <div className={styles.container}>
         <h2>Subtraction</h2>
         <GameLink type="subtraction" amount={1} text="+1" />
         <GameLink type="subtraction" amount={2} text="+2" />
