a1a74d9e0b3ffc385ab03d13373f731c52306120
jacquelyn-bradshaw <jacquelyn.bradshaw@flutteruki.com>
2025-01-03 11:55:40 +0000

    added base styles to blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,19 @@
 import Link from "next/link";
 
+import styles from "./page.module.scss";
+
 export default function Blog() {
   return (
-    <main>
+    <main className={styles.main}>
       <h1>Blog</h1>
-      <Link href="/blog/first-post">First Post</Link>
-      <Link href="/blog/second-post">Second Post</Link>
+      <div className={styles.links}>
+        <Link href="/blog/first-post" className={styles.link}>
+          First Post
+        </Link>
+        <Link href="/blog/second-post" className={styles.link}>
+          Second Post
+        </Link>
+      </div>
     </main>
   );
 }
