93e684f1eafdc7569be2380268d805d5d858aa31
jacquelyn-bradshaw <jacquelyn.bradshaw@flutteruki.com>
2025-01-03 09:55:39 +0000

    background image added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Header from "@/components/header/header";
 
 export const metadata: Metadata = {
-  title: "My App",
+  title: "Number Ride",
   description: "",
 };
 
