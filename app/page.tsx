79f7d4bf36d2a001e7074baa7402b26fc44e3113
jacquelyn-bradshaw <jacquelyn.bradshaw@flutteruki.com>
2025-01-03 11:11:49 +0000

    exported the game Links into a separate component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Link from "next/link";
-import Button from "@/components/button/button";
+import GameLink from "@/components/gameLink";
 
 import styles from "./page.module.css";
 
@@ -8,28 +7,9 @@ export default function Home() {
     <main className={styles.main}>
       <div>
         <h2>Addition</h2>
-        <Link
-          href={{
-            pathname: "/game",
-            query: {
-              type: "addition",
-              amount: 1,
-            },
-          }}
-        >
-          <Button text="+1" />
-        </Link>
-        <Link
-          href={{
-            pathname: "/game",
-            query: {
-              type: "addition",
-              amount: 2,
-            },
-          }}
-        >
-          <Button text="+2" />
-        </Link>
+        <GameLink type="addition" amount={1} text="+1" />
+        <GameLink type="addition" amount={2} text="+2" />
+        <GameLink type="addition" amount={3} text="+3" />
       </div>
     </main>
   );
