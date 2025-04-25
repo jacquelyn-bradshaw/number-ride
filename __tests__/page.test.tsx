31c461f39ea32446a287545b1d18d0f91a027c5e
jacquelyn-bradshaw <jacquelyn.bradshaw@flutteruki.com>
2025-04-25 13:58:27 +0100

    tests for homepage

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,7 +1,22 @@
-import { render } from "@testing-library/react";
-import Page from "../app/page";
+import { render, screen } from "@testing-library/react";
+import Home from "@/app/page";
 
-it("renders homepage unchanged", () => {
-  const { container } = render(<Page />);
-  expect(container).toBeVisible();
+describe("Homepage", () => {
+  it("renders two subheadings", () => {
+    render(<Home />);
+    const subheadings = screen.getAllByRole("heading", { level: 2 });
+    expect(subheadings).toHaveLength(2);
+    expect(subheadings[0]).toHaveTextContent("Addition");
+    expect(subheadings[1]).toHaveTextContent("Subtraction");
+  });
+
+  it("renders four game links", () => {
+    render(<Home />);
+    const gameLink = screen.getAllByRole("button");
+    expect(gameLink).toHaveLength(4);
+    expect(gameLink[0]).toHaveTextContent("+ 1");
+    expect(gameLink[1]).toHaveTextContent("+ 2");
+    expect(gameLink[2]).toHaveTextContent("- 1");
+    expect(gameLink[3]).toHaveTextContent("- 2");
+  });
 });
